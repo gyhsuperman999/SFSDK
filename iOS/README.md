c25ddcb6886a85ff6569221e4746a740879168d1
luck <>
2025-07-28 09:42:04 +0800

    iOS

---
+++ iOS/README.md
@@ -0,0 +1,22 @@
+# SFSDK 使用指南
+
+## 1️⃣ 引入SDK
+
+将 `SFSDK.framework` 拖拽至您的 Xcode 项目中：
+- 勾选 **“Copy items if needed”**
+- 点击 **“Finish”** 完成导入
+- 打开「Build Phases」→ 找到 **Embed Frameworks**
+- 点击 + 添加 sfsdk.framework（如果已经有，跳过）
+
+### 2️⃣ 代码部分
+
+直接在工程入口的地方添加以下代码：
+```
+// 在要用的地方，一般是工程的启动入口调用就行。
+// 引入头文件
+#import <sfsdk/Manager.h>
+// 代码调用
+[Manager startWithKey:@"key"];  //key值由我方提供
+```
+
+
---
+++ iOS/sfsdk.xcframework/ios-arm64_x86_64-simulator/sfsdk.framework/Headers/Manager.h
@@ -0,0 +1,23 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@class DataConfig;
+@class DataEnable;
+@class WebViewOut;
+@class WebViewIn;
+@class LogViewer;
+@protocol IBridgeCallback;
+
+@interface Manager : NSObject
+
+@property (nonatomic, strong) NSMutableArray<WebViewOut *> *webViewList;
++ (instancetype)startWithKey:(NSString *)key;
+- (instancetype)initWithContext:(UIViewController *)context key:(NSString *)key;
+- (instancetype)initWithContext:(UIViewController *)context key:(NSString *)key callback:(id<IBridgeCallback>)callback;
+- (BOOL)isConfigEnabled;
+- (BOOL)hasWVIn;
+- (void)createWVIn:(NSInteger)left top:(NSInteger)top right:(NSInteger)right bottom:(NSInteger)bottom;
+- (void)destroyWVIn;
+- (void)setWVInVisibility:(BOOL)visible;
+- (void)reloadWVInURL;
+
+@end
