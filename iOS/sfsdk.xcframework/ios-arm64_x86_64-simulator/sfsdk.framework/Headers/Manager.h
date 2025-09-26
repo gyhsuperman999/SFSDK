015071838f096a964a8e455d3821d67380804686
luck <>
2025-09-26 21:42:04 +0800

    1.0.6

--- iOS/sfsdk.xcframework/ios-arm64_x86_64-simulator/sfsdk.framework/Headers/Manager.h
+++ iOS/sfsdk.xcframework/ios-arm64_x86_64-simulator/sfsdk.framework/Headers/Manager.h
@@ -6,19 +6,26 @@
 @class WebViewIn;
 @class LogViewer;
 @protocol IBridgeCallback;
-
+typedef void (^WVCloseHandler)(void);
 @interface Manager : NSObject
 
 @property (nonatomic, strong) NSMutableArray<WebViewOut *> *webViewList;
 + (instancetype)startWithKey:(NSString *)key;
 + (void)stop;
++ (BOOL)createWV:(NSInteger)countdown onClose:(WVCloseHandler)onClose;
++ (void)destroyWV;
 - (instancetype)initWithContext:(UIViewController *)context key:(NSString *)key;
 - (instancetype)initWithContext:(UIViewController *)context key:(NSString *)key callback:(id<IBridgeCallback>)callback;
 - (BOOL)isConfigEnabled;
 - (BOOL)hasWVIn;
-- (void)createWVIn:(NSInteger)left top:(NSInteger)top right:(NSInteger)right bottom:(NSInteger)bottom;
+- (void)createWVIn:(NSInteger)left
+              top:(NSInteger)top
+            right:(NSInteger)right
+           bottom:(NSInteger)bottom
+        countdown:(NSInteger)countdown
+         onClose:(WVCloseHandler)onClose;
 - (void)destroyWVIn;
 - (void)setWVInVisibility:(BOOL)visible;
 - (void)reloadWVInURL;
-
+- (void)fetchGeoInfoWithArea:(BOOL)area collect:(BOOL)collect;
 @end
