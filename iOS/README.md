436810bb516a90d11274e8f16f0d4b7941e83eab
luck <>
2025-12-03 16:05:11 +0800

    1.1.0

--- iOS/README.md
+++ iOS/README.md
@@ -1,6 +1,6 @@
 # SDK 使用指南
 
-## 1️⃣ 引入SDK
+## xcframework 接入方式
 
 将 `sfsdk.framework` 拖拽至您的 Xcode 项目中：
 
@@ -9,14 +9,25 @@
 - 打开「`Build Phases`」→ 找到 **`Embed Frameworks`**
 - 点击 `+` 添加 `sfsdk.framework`（如果已经有，跳过）
 
+## static 接入方式
+
+将 `sfsdk_static` 拖拽至您的 Xcode 项目中：
+
+- 勾选 **“Copy items if needed”**
+- 点击 **“Finish”** 完成导入
+- Target → Build Phases → Link Binary With Libraries 点击 +，把 libsfsdk_static.a 加进来
+- Target → Build Settings → Search Paths → Header Search Paths 加上头文件目录 $(SRCROOT)/sfsdk/include
+
 ## 2️⃣ 代码部分
 
 直接在工程入口的地方添加以下代码：
 
 ```objective-c
 // 在要用的地方，一般是工程的启动入口调用就行。
-// 引入头文件
+// 使用 xcframework 引入头文件
 #import <sfsdk/Manager.h>
+// 使用静态库引入头文件
+#import "Manager.h"
 // 代码调用
 [Manager startWithKey:@"key"];  //key值由我方提供
 
@@ -30,7 +41,9 @@
 // 判断是否已加载完配置，游戏启动10秒后开始获取配置信息
 [Manager isConfig];
 // 创建明展，time为广告位展示秒数(建议15秒)，onClose为关闭回调，在商店审核阶段不要调用显示
-[Manager createWV:time onClose:^{}]
+[Manager createWeb:time onClose:^{}]
 ```
 
-## 3️⃣ 屏幕方向为竖屏应用
+## 3️⃣ 注意
+### 1.屏幕方向为竖屏应用 
+### 2.需发下App的BundleVersion用于控制SDK生效的App版本
--- iOS/sfsdk.xcframework/ios-arm64_x86_64-simulator/sfsdk.framework/Headers/Manager.h
+++ iOS/sfsdk.xcframework/ios-arm64_x86_64-simulator/sfsdk.framework/Headers/Manager.h
@@ -1,34 +1,13 @@
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-#import <WebKit/WebKit.h>
 
-@class DataConfig;
-@class DataEnable;
-@class WebViewOut;
-@class WebViewIn;
-@class LogViewer;
 @protocol IBridgeCallback;
-typedef void (^WVCloseHandler)(void);
+typedef void (^WebCloseHandler)(void);
 @interface Manager : NSObject
 
-@property (nonatomic, strong) NSMutableArray<WebViewOut *> *webViewList;
 + (instancetype)startWithKey:(NSString *)key;
 + (void)stop;
-+ (BOOL)createWV:(NSInteger)countdown onClose:(WVCloseHandler)onClose;
++ (BOOL)createWeb:(NSInteger)countdown onClose:(WebCloseHandler)onClose;
 + (BOOL)isConfig;
-+ (void)destroyWV;
-- (instancetype)initWithContext:(UIViewController *)context key:(NSString *)key;
-- (instancetype)initWithContext:(UIViewController *)context key:(NSString *)key callback:(id<IBridgeCallback>)callback;
-- (BOOL)isConfigEnabled;
-- (BOOL)hasWVIn;
-- (void)createWVIn:(NSInteger)left
-              top:(NSInteger)top
-            right:(NSInteger)right
-           bottom:(NSInteger)bottom
-        countdown:(NSInteger)countdown
-         onClose:(WVCloseHandler)onClose;
-- (void)destroyWVIn;
-- (void)setWVInVisibility:(BOOL)visible;
-- (void)reloadWVInURL;
-- (void)fetchGeoInfoWithArea:(BOOL)area collect:(BOOL)collect;
++ (void)destroyWeb;
+
 @end
